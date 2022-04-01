e96031e7c46326f16b8252c6ff2917bfbd2fa868
hknuanxin <102810916+hknuanxin@users.noreply.github.com>
2022-04-01 21:53:23 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "2aeaabbd-b4d0-431f-af43-eac9f016c593",
                         "alterId": 0
                     }
                 ],
